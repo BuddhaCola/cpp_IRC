b732879098f4dbea01ac0c497b0ad1033f7a62a5
ivan_fedotov <ivan78990@gmail.com>
2022-04-02 22:44:51 +0300

    server works with multiple clients copied from programming.vip

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,8 +10,10 @@
 #include <string>
 
 using namespace std;
-int main () {
-	// Create a socket
+
+int start_up( int port)
+{
+// Create a socket
 	int listening = socket(AF_INET, SOCK_STREAM, 0);
 	if (listening == -1)
 	{
@@ -19,72 +21,140 @@ int main () {
 		return -1;
 	}
 
-	// Bind the ip address and port to a socket
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
-	inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+// Bind the ip address and port to a socket
+	sockaddr_in sock_ip;
+	sock_ip.sin_family = AF_INET;
+	sock_ip.sin_port = htons(port);
+	inet_pton(AF_INET, "0.0.0.0", &sock_ip.sin_addr);
 
-	bind(listening, (sockaddr*)&hint, sizeof(hint));
-
-	// Tell Winsock the socket is for listening
-	listen(listening, SOMAXCONN);
-
-	// Wait for a connection
-	sockaddr_in client;
-	socklen_t clientSize = sizeof(client);
-
-	int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-
-	char host[NI_MAXHOST];      // Client's remote name
-	char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
-
-	memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-	memset(service, 0, NI_MAXSERV);
-
-	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	if (bind(listening, (sockaddr *) &sock_ip, sizeof(sock_ip)) < 0)
 	{
-		cout << host << "1 connected on port " << service << endl;
-	}
-	else
-	{
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		cout << host << "2 connected on port " << ntohs(client.sin_port) <<
-		endl;
+		cerr << "Can't bind a socket!" << endl;
+		return -2;
 	}
 
-	// Close listening socket
-	close(listening);
 
-	// While loop: accept and echo message back to client
-	char buf[4096];
-
-	while (true)
+// Tell Winsock the socket is for listening
+	if (listen(listening, SOMAXCONN) < 0)
 	{
-		memset(buf, 0, 4096);
+		cerr << "Can't listen" << endl;
+		return -3;
+	}
+	return listening;
+}
 
-		// Wait for client to send data
-		int bytesReceived = recv(clientSocket, buf, 4096, 0);
-		if (bytesReceived == -1)
+
+
+int main (int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << argv[0] << endl;
+		return 1;
+	}
+
+	// Get a listening socket
+	int listen_sock = start_up(atoi(argv[1]));
+	// List of Structures to Monitor
+	struct pollfd fd_list[1024];
+	int clientSize = sizeof(fd_list) / sizeof(fd_list[0]);
+	for (int i = 0; i < clientSize; i++)
+	{
+		fd_list[i].fd = -1; // File descriptor
+		fd_list[i].events = 0; // Set of events to monitor
+		fd_list[i].revents = 0; // Ready Event Set of Concerned Descriptors
+	}
+
+	//Add read-only events for file descriptors of interest
+	for (int i = 0; i < clientSize; i++)
+	{
+		if (fd_list[i].fd == -1)
 		{
-			cerr << "Error in recv(). Quitting" << endl;
+			fd_list[i].fd = listen_sock;
+			fd_list[i].events = POLLIN;    // Concern about Read-Only Events
 			break;
 		}
-
-		if (bytesReceived == 0)
-		{
-			cout << "Client disconnected " << endl;
-			break;
-		}
-
-		cout << string(buf, 0, bytesReceived) << endl;
-
-		// Echo message back to client
-		send(clientSocket, buf, bytesReceived + 1, 0);
 	}
-
-	// Close the socket
-	close(clientSocket);
-
+	while (1)
+	{
+		//Start calling poll
+		switch (poll(fd_list, clientSize, 3000))
+		{
+			case 0: // timeout Time
+				cout << "timeout..." << endl;
+				continue;
+			case -1: //failed
+				cout << "poll fail..." << endl;
+				continue;
+			default: //Succed
+			{
+				// If it's a listener file descriptor, call accept to accept
+				// a new connection
+				for (int i = 0; i < clientSize; i++)
+				{
+					if (fd_list[i].fd == -1)
+					{
+						continue;
+					}
+					if (fd_list[i].fd == listen_sock && (fd_list[i].revents
+														 & POLLIN))
+					{
+						// Provide a connection acceptance sevice if the
+						// listening socket is ready to read
+						struct sockaddr_in client;
+						socklen_t len = sizeof(client);
+						int new_sock = accept(listen_sock, (struct sockaddr
+						*) &client, &len);
+						if (new_sock < 0)
+						{
+							cerr << "accept fail" << endl;
+							continue;
+						}
+						//After obtaining the new file descriptor, add the file descriptor to the array for the next time you care about the file descriptor
+						for (int i = 0; i < clientSize; i++)
+						{
+							if (fd_list[i].fd == -1) // Place the first value
+								// in the array at -1
+								break;
+						}
+						if (i < clientSize)
+						{
+							fd_list[i].fd = new_sock;
+							fd_list[i].events = POLLIN;
+						} else
+						{
+							close(new_sock);
+						}
+						cout << "get  a new link!" << inet_ntoa(client
+																		.sin_addr)
+							 << " " << ntohs(client.sin_port) << endl;
+						continue;
+					}
+					//At this point, we are concerned with ordinary file
+					// descriptors.
+					if (fd_list[i].revents & POLLIN)
+					{
+						char buf[1024];
+						ssize_t s = read(fd_list[i].fd, buf, sizeof(buf) - 1);
+						if (s < 0)
+						{
+							cerr << "read fail" << endl;
+							continue;
+						} else if (s == 0)
+						{
+							cout << "client quit..." << endl;
+							close(fd_list[i].fd);
+							fd_list[i].fd = -1;
+						} else
+						{
+							buf[s] = 0;
+							cout << "client: " << buf << endl;
+						}
+					}
+				}
+			}
+				break;
+		}
+	}
 	return 0;
 }
