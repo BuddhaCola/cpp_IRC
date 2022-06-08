782728a2f7f858fc0fde3c3f8f42c7b1b6ccaeb9
ivan_fedotov <ivan78990@gmail.com>
2022-06-08 15:38:20 +0300

    ping uncommented

--- srcs/PingClient.cpp
+++ srcs/PingClient.cpp
@@ -6,16 +6,16 @@
 
 void Server::pingClient()
 {
-//	std::time_t timeNow;
-//	std::string pingString;
-//	for (int i = 0; i < _users.size(); i++) {
-//		User &user  = *_users.at(i);
-//		pingString = "PING " + std::to_string(user.getTimestamp()) + "\r\n";
-//		logger.logUserMessage(pingString, user, OUT);
-//		write(user.getFd(), pingString.c_str(), pingString.length());
-//		timeNow = std::time(NULL);
-//		if (timeNow - user.getTimestamp() >= TIMEOUT) {
-//			killUser(user);
-//		}
-//	}
+	std::time_t timeNow;
+	std::string pingString;
+	for (int i = 0; i < _users.size(); i++) {
+		User &user  = *_users.at(i);
+		pingString = "PING " + std::to_string(user.getTimestamp()) + "\r\n";
+		logger.logUserMessage(pingString, user, OUT);
+		write(user.getFd(), pingString.c_str(), pingString.length());
+		timeNow = std::time(NULL);
+		if (timeNow - user.getTimestamp() >= TIMEOUT) {
+			killUser(user);
+		}
+	}
 }
